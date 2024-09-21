72ce28e15a2360d2f11db13428b863799601b788
Kim <72332310+kimnle@users.noreply.github.com>
2024-09-21 10:42:31 +1000

    feat: navbar with links

---
+++ src/App_ObjectRouter.js
@@ -0,0 +1,31 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+
+// Convert an array of objects into a <Routes> componenet containing <Route> components
+const someFancyRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/about",
+    element: <AboutPage />
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />
+  }
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={someFancyRouter} />
+    </div>
+  );
+}
+
+export default App;
